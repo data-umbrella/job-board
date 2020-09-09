c3b3750a6e583b35969922ff2bda2ab807607fc6
Ty Shaikh <me@tyshaikh.com>
2020-09-09 08:56:10 -0400

    Fixed domain.rb with new folder structure

--- workers/domains.rb
+++ workers/domains.rb
@@ -4,42 +4,48 @@ require "ostruct"
 
 host_names = []
 
-accounts = YAML::Store.new "../data/accounts.store"
 domains = YAML::Store.new "../data/domains.store"
+paths = Dir.glob("../data/**/settings.store")
 
-accounts.transaction(true) do
-  accounts.roots.each do |data_root_name|
+paths.each do |path|
+  split_path = path.split('/')
+  settings_store = YAML::Store.new path
 
-    account = accounts[data_root_name]
-    slug = account.slug
-    tmp_domain = account.domain
+  settings_store.transaction(true) do
+    settings_store.roots.each do |data_root_name|
+      settings = settings_store[data_root_name]
 
-    if slug and tmp_domain
-      p slug, tmp_domain
+      slug = settings.slug
+      tmp_domain = settings.domain
 
-      if tmp_domain == ''
-        next
-      end
+      if slug and tmp_domain
+        p slug, tmp_domain
 
-      # check if subdomain or regular domain and remove www
-      if tmp_domain.include? 'www.'
-        tmp_domain.slice! 'www.'
-        domain = tmp_domain
-        type = 'domain'
-      elsif tmp_domain.scan(/\./).length >= 2
-        domain = tmp_domain
-        type = 'subdomain'
-      else
-        domain = tmp_domain
-        type = 'domain'
-      end
+        if tmp_domain == ''
+          next
+        end
+
+        # check if subdomain or regular domain and remove www
+        if tmp_domain.include? 'www.'
+          tmp_domain.slice! 'www.'
+          domain = tmp_domain
+          type = 'domain'
+        elsif tmp_domain.scan(/\./).length >= 2
+          domain = tmp_domain
+          type = 'subdomain'
+        else
+          domain = tmp_domain
+          type = 'domain'
+        end
 
 
-      domains.transaction do
-        domain_obj = OpenStruct.new(domain: domain, slug: slug, type: type)
-        domains[slug] = domain_obj
+        domains.transaction do
+          domain_obj = OpenStruct.new(domain: domain, slug: slug, type: type)
+          domains[slug] = domain_obj
+        end
       end
 
     end
   end
+
 end
